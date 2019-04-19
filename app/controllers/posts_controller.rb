686bd4cd6634f6098a0ba9db2f483aca469fe40b
kuroki0518 <kurokiniu@gmail.com>
2019-04-19 17:04:44 +0900

    postの基本的なcrud機能を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,49 @@
 class PostsController < ApplicationController
+  before_action :set_target_post, only: %i[show edit update destroy]
   def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      redirect_to posts_path
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @post.update(post_params)
+    if post.save
+      redirect_to post
+    else
+      redirect_to edit_post_path(post.id)
+    end
+  end
+
+  def destroy
+    @post.delete
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:name, :title, :content)
+  end
+
+  def set_target_post
+    @post = Post.find(params[:id])
   end
 end
---
+++ spec/factories/posts.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :post do
+    name { "MyString" }
+    title { "MyString" }
+    content { "MyText" }
+  end
+end
